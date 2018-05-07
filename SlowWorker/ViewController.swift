e20bd6733175a88f70226694fcabd844218f5757
AbdelRahman Aref <abdelrahman_aref@hotmail.com>
2018-05-07 15:37:45 +0200

    Final product and using of DispatchGroup and notify in background along with Dipatch main thread

--- SlowWorker/ViewController.swift
+++ SlowWorker/ViewController.swift
@@ -10,15 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var resutltsTextView: UITextView!
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    func fetchSomethingFromServer() -> String {
+        Thread.sleep(forTimeInterval: 1)
+        return "Hi There"
+    }
+    func processData(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 2)
+        return data.uppercased()
+    }
+    func calculateFirstResult(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 3)
+        return "Number of chars: \(data.count)"
+    }
+    func calculateSecondResult(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 4)
+        return data.replacingOccurrences(of: "E", with: "e")
+    }
+    
+    @IBAction func doWork(_ sender: Any) {
+        let startTime = Date()
+        self.resutltsTextView.text = ""
+        startButton.isEnabled = false
+        spinner.startAnimating()
+        let queue = DispatchQueue.global(qos: .default)
+        queue.async {
+            let fetchedData = self.fetchSomethingFromServer()
+            let processedData = self.processData(fetchedData)
+            var firstResult : String!
+            var secondResult : String!
+            let group = DispatchGroup()
+            let group2 = DispatchGroup()
+            queue.async(group : group) {
+                secondResult = self.calculateSecondResult(processedData)
+            }
+            queue.async(group: group2){
+                firstResult = self.calculateFirstResult(processedData)
+
+            }
+            group.notify(queue: queue){
+              let resultsSummary = "First: [\(firstResult!)]\nSecond: [\(secondResult!)]"
+            DispatchQueue.main.async {
+                self.resutltsTextView.text = resultsSummary
+                self.startButton.isEnabled = true
+                self.spinner.stopAnimating()
+            }
+            let endTime = NSDate()
+            print("completed in \(endTime.timeIntervalSince(startTime as Date)) seconds")
+        }
+      }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
